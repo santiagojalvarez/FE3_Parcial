65fd8b0bba257446df61f5a59002ca1cdad18939
santiagoAlvarez <santiago.j.alvarez88@gmail.com>
2023-05-31 20:32:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Carga de estudiants</h1>
+      <form>
+        <label></label>
+        <input type="text" />
+
+        <label></label>
+        <input type="text" />
+
+        <label></label>
+        <input type="text" />
+
+        <select >
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </form>
+
+      <Card/>
+      
+    </div>
+  )
+}
+
+export default App
