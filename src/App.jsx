ee20c2d1b423c312a9fdf37be494ea82d3123235
santiagoAlvarez <santiago.j.alvarez88@gmail.com>
2023-05-31 22:38:35 -0300

    entrega del parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 
+import { useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
 
 function App() {
+  const [usuario,setUsuario] = useState({
+    nombre:"",
+    color:""
+  })
+
+  const[show, setShow] = useState(false)
+  const [error, setError] = useState(false)
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    usuario.nombre.length > 3 && usuario.nombre[0] !== " " && usuario.color.length >= 6 ? setShow(true) : setError(true)
+  }
+
+
 
   return (
     <div className='App'>
-      <h1>Carga de estudiants</h1>
-      <form>
-        <label></label>
-        <input type="text" />
+      <h1>Carga de estudiantes</h1>
+      <form onSubmit={handleSubmit} className='form'>
+        <label>Ingresa tu nombre:</label>
+        <input type="text" onChange={(e)=> setUsuario({...usuario, nombre: e.target.value})}/>
 
-        <label></label>
-        <input type="text" />
-
-        <label></label>
-        <input type="text" />
-
-        <select >
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-        </select>
+        <label>Ingresa tu color favorito (formato HEX):</label>
+        <input type="text" onChange={(e)=> setUsuario({...usuario,color: e.target.value})}/>
+        <button type='submit'>ENVIAR</button>
       </form>
 
-      <Card/>
-      
+      { show && <Card nombre={usuario.nombre} color={usuario.color}/>}
+      {error && <p className='error'>Por favor chequea que la informacion sea correcta</p>}
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({nombre, color}) => {
     return (
-        <div>Hola, soy una tarjeta</div>
+        <div className='card'>
+            <h1>Hola {nombre}</h1>
+            <h3>Sabemos que tu color favorito es: {color}</h3>
+        </div>
     )
 }
 
